3090163999cf0059555221ab899fa27979874e8c
Joshua Uwagwu <chuksjosh20@gmail.com>
2022-04-24 00:35:37 +0100

    added PlatformController and automapper profiles

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -32,6 +32,7 @@ namespace PlatformService
             
             services.AddScoped<IPlatformRepo, PlatformRepo>();
             services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
@@ -58,6 +59,8 @@ namespace PlatformService
             {
                 endpoints.MapControllers();
             });
+            
+            PrepDb.PrepPopulation(app);
         }
     }
 }
